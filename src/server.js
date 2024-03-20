13ab9d5e3946fee46e0e747955bee16cb6e329d6
Chloe Yeo <yeochloe01@gmail.com>
2024-03-20 12:21:47 +0900

    put request implemented

--- src/server.js
+++ src/server.js
@@ -104,11 +104,21 @@ const server = async function () {
     app.put("/user/:userId", async function (req, res) {
       try {
         const { userId } = req.params; // same as getting req.params.userId
-        const { username, name, first, last, age, email } = req.body;
+        const { age, email } = req.body; // need to do this first before setting
         const user = await User.findByIdAndUpdate(
-          { _id: userId },
+          userId,
+          //   { _id: userId },
           //   { $set: req.body },
-          { $set: { username, age, name.first: first, email } },
+          // if not included in $set, the put request itself gets accepted
+          // but e.g. username does not actually gets changed if it wasn't in $set
+          // if set both age and email, can only send either one or both in put
+          // can only set age, can only set email, can set both, these will all work
+          // when trying to set something else, put request will send 200 ok but
+          // that other field won't actually be changed
+          { $set: { age, email } }, // same as age: age, email: email
+          //{ $set: { age: age, email: email } }, // same as above!
+          // cannot do e.g. ageHi:age - then it will send 200 OK however it
+          // won't recognise the ageHi field in schema thus age won't be changed
           { new: true }
         );
         return res.send({ user: user });
